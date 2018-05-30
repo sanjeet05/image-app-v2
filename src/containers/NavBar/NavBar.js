860f3421ac878d7b4e2c4d4e88cf4d48eb6c7424
Sanjeet <sanjeet.kumar@zylotech.com>
2018-05-30 18:48:11 +0530

    active link

--- src/containers/NavBar/NavBar.js
+++ src/containers/NavBar/NavBar.js
@@ -1,10 +1,6 @@
 import React, { Component, Fragment } from "react";
 import { Link, NavLink } from "react-router-dom";
-import {  
-  Navbar,
-  Nav,
-  NavItem,
-} from "react-bootstrap";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
 
 // import css
 import "./NavBar.css";
@@ -13,44 +9,45 @@ class NavBar extends Component {
   state = {};
 
   render() {
-    return (      
-      <Fragment>         
-        <Navbar fixedTop className="top_navbar">
+    return (
+      <Fragment>
+        <Navbar collapseOnSelect fixedTop className="top_navbar">
           <Navbar.Header>
             <Navbar.Brand>
               <Link to="/">
-                <div className="top_logo_name">
-                  My Gallery
-                </div>
+                <div className="top_logo_name">My Gallery</div>
               </Link>
             </Navbar.Brand>
+            <Navbar.Toggle />
           </Navbar.Header>
-          
-          <Nav>
-            <NavItem
-              eventKey={1}
-              componentClass={NavLink} 
-              href="/task_one" 
-              to="/task_one"
-              activeStyle={{ color: '#333333' }}
-              >
-              Task One
-            </NavItem>
-            <NavItem
-              eventKey={2}
-              componentClass={NavLink} 
-              href="/task_two" 
-              to="/task_two"
-              activeStyle={{ color: '#333333' }}
-              >
-              Task Two
-            </NavItem>
-          </Nav>
 
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem
+                eventKey={1}
+                componentClass={NavLink}
+                href="/task_one"
+                to="/task_one"
+                activeStyle={{ color: "#333333" }}
+              >
+                Task One
+              </NavItem>
+              <NavItem
+                eventKey={2}
+                componentClass={NavLink}
+                href="/task_two"
+                to="/task_two"
+                activeStyle={{ color: "#333333" }}
+              >
+                Task Two
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+          
         </Navbar>
-      </Fragment>    
+      </Fragment>
     );
   }
-};
+}
 
 export default NavBar;
